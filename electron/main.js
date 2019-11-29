19568919912bda567c752a7e386cc615bafa6fc0
Marco Goebel <marco.goebel.1@student.fhws.de>
2019-11-29 07:21:22 +0100

    integrate server project with react

--- electron/main.js
+++ electron/main.js
@@ -1,15 +1,19 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const { channels } = require("../src/shared/constants");
+const { createServer } = require("../server/server");
 const path = require("path");
 const url = require("url");
 
 let mainWindow;
+let server;
 
 function createWindow() {
+  // important for deployment -> delete path "build" 
+  // -> https://stackoverflow.com/questions/41130993/electron-not-allowed-to-load-local-resource
   const startUrl =
     process.env.ELECTRON_START_URL ||
     url.format({
-      pathname: path.join(__dirname, "../index.html"),
+      pathname: path.join(__dirname, "../build/index.html"),
       protocol: "file:",
       slashes: true
     });
@@ -26,7 +30,10 @@ function createWindow() {
   });
 }
 
-app.on("ready", createWindow);
+app.on("ready", () => {
+  server = createServer();
+  createWindow();
+});
 
 app.on("window-all-closed", function() {
   if (process.platform !== "darwin") {
